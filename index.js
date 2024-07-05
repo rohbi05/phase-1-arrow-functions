09d431561c970c56ab4e3c5b7c3e4c837b66d16c
shantal rohbi <shantal.rohbi@student.moringaschool.com>
2024-07-05 12:16:05 +0300

    completed arrow function assighnments

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 2000;
+let b = 100;
+function divide(a,b){
+    return a/b;
+}
+let x = 2;
+const square = (x) =>{
+    return x * x;
+}
+let c = 3;
+let d = 4;
+const add = (c,d) =>{
+    return c + d;
+
+}
